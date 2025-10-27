f47fb1bc05cea23c0abf32e5aacb1e370371f0d2
BartekD130 <bartosz.drwal.bd@gmail.com>
2025-10-27 10:35:48 +0000

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,18 @@
 
 public class Main {
   public static void main(String[] args) {
-    int a = 5;
-    System.out.println("Wartość zmiennej to: " +  a);
+    double a = 5;
+    double b = 10;
+    double suma = a + b;
+    double roznica = a - b;
+    double iloczyn = a * b;
+    double iloraz = a / b;
+    System.out.println("Wartość a: " + a);
+    System.out.println("Wartość b: " + b);
+    System.out.println("Wartość dodawania: " + suma);
+    System.out.println("Wartość odejmowania: " + roznica);
+    System.out.println("Wartość mnożenia: " + iloczyn);
+    System.out.println("Wartość dzielenia: " + iloraz);
   }
 
   // @Test
