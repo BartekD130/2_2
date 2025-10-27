4316f93f6af0bcec373a900c66c8a0f911783523
BartekD130 <bartosz.drwal.bd@gmail.com>
2025-10-27 10:10:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    int a = 5;
+    System.out.println("Wartość zmiennej to: " +  a);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
